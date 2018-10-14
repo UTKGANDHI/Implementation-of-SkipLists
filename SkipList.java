d126fefc9772912f095740b33339f7f43adf95ed
eshapunjabi8888 <eshapunjabi73@gmail.com>
2018-10-13 19:31:05 -0500

    Update SkipList.java

--- SkipList.java
+++ SkipList.java
@@ -1,9 +1,7 @@
 /* Starter code for LP2 */
 
 // Change this to netid of any member of team
-package krt170130;
-
-import sun.awt.X11.XConstants;
+package EHP170000;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -13,22 +11,24 @@ import java.util.Random;
 
 public class SkipList<T extends Comparable<? super T>> {
     static final int PossibleLevels = 33;
-    Entry <T> head, tail;
+    Entry<T> head,tail;//dummy nodes
     int size, maxLevel;
-    Entry <T> last[];
+    Entry[] last; //used by find
     Random random;
+
+
+
 
     static class Entry<E> {
         E element;
         Entry[] next;
         Entry prev;
-        int[] span; // for indexing
+        int[] span; //for indexing
 
         public Entry(E x, int lev) {
             element = x;
             next = new Entry[lev];
-            span = new int[lev]; // span array stores the width as in count of the nodes that it jumps at level i. This is used for searching at a position.
-            // add more code if needed
+            span=new int[lev];
         }
 
         public E getElement() {
@@ -38,269 +38,139 @@ public class SkipList<T extends Comparable<? super T>> {
 
     // Constructor
     public SkipList() {
-
-        this.head = new Entry<>(null, 33);
-        this.tail = new Entry<>(null, 33);
-        this.size = 0;
-        this.maxLevel = 1;
-        this.last = new Entry[33];
-        this.random = new Random();
-
-        // Initialize last of each level at head so that in case in add method when we call chooselevel after add it won't throw any exception if maxLevel increases by chooselevel.
-        setup();
-    }
-    private void setup() {
-        for (int i=0;i<33;i++) {
-            this.last[i] = this.head;
-            this.head.next[i] = this.tail;
+        head=new Entry(null,33);
+        tail=new Entry(null,33);
+        size=0;
+        maxLevel=1;
+        last=new Entry[33];
+        random=new Random();
+        //initialize next and span arrays of head
+        for(int i=0;i<33;i++)
+        {
+            head.span[i]=0;
+            head.next[i]=tail;
+            last[i]=head;
         }
-    }
 
-    private void find(T x) {
-        Entry curr = this.head; // Start from head and go down until lowest level filling up the last array such that the jump at last[i] doesn't cross x
-        for (int i = this.maxLevel -1;i>=0;i--) {
-            while(curr.next[i].element!= null && x.compareTo((T)curr.next[i].element) > 0) { // checking if the next element is tail or not
-                curr = curr.next[i];
-            }
-            this.last[i] = curr;
-        }
-    }
-
-    public void printSpanArray(int n) {
-        int index = 0;
-        Entry curr = this.head;
-        while(index++ < n) {
-            curr = curr.next[0];
-        }
-        System.out.println(curr.element);
-        int arr[] = curr.span;
-        for (int i=0;i<curr.span.length;i++) {
-            System.out.print(curr.span[i] + " ");
-        }
-        System.out.println();
-    }
-
-    private int[] findWithSpan(T x, int level) {
-        int[] spanLevel = new int[level];
-        Entry curr = this.head;
-        for (int i=level - 1;i>=0;i--) {
-            while(curr.next[i].element != null && x.compareTo((T)curr.next[i].element) > 0) {
-                spanLevel[i] += curr.span[i];
-                curr = curr.next[i];
-            }
-        }
-        return spanLevel;
     }
 
     // Add x to list. If x already exists, reject it. Returns true if new node is added to list
     public boolean add(T x) {
-        if(contains(x)) return false;
-        int level = chooseLevel();
-//        System.out.println(x + " " + level);
-        int[] levelSpan = findWithSpan(x, level);
-        int currSpan = 0;
-        Entry ent = new Entry(x, level);
-        for (int i=0;i<level;i++) {
-            ent.next[i] = this.last[i].next[i];
-            this.last[i].next[i] = ent;
-            ent.span[i] = this.last[i].span[i] - currSpan;
-            this.last[i].span[i] = currSpan + 1;
-            currSpan += levelSpan[i];
+        int lev=chooseLevel();
+        Entry<T> ent=new Entry<T>(x,lev);
+        int[] steps_span=new int[maxLevel];
+        //if element already exists don't add it
+        if(containsWithSpan(x,steps_span))
+            return false;
+
+        int steps=0;
+
+        //update pointers and spans of elements in last array as well as current element
+        for(int i=0;i<ent.next.length;i++)
+        {
+            ent.next[i]=last[i].next[i];
+            last[i].next[i]=ent;
+            ent.span[i]=last[i].span[i]-steps;
+            last[0].span[0]=steps+1;
+            steps+=steps_span[i];
         }
 
-        for(int i = level; i<this.maxLevel;i++) {
-            this.last[i].span[i]++;
+        //update spans for the rest of the next pointers
+        for(int i=ent.next.length;i<maxLevel;i++)
+        {
+            last[i].span[i]=last[i].span[i]+1;
         }
-        /*System.out.println(this.maxLevel);
-        for (int i=0;i<this.maxLevel;i++) {
-            System.out.print(this.head.span[i] +  " ");
-        }
-        System.out.println();*/
-        ent.next[0].prev = ent;
-        ent.prev = this.last[0];
-        this.size++;
+
+        ent.next[0].prev=ent;
+        ent.prev=last[0];
+        size+=1;
         return true;
-    }
-
-    private int chooseLevel() {
-        int level =1;
-        level = 1 + Integer.numberOfTrailingZeros(random.nextInt());
-        level = Math.min(level, this.maxLevel + 1);
-        if(level > this.maxLevel){
-            for (int i=this.maxLevel;i<level;i++) {
-                this.head.span[i] = this.size;
-            }
-            this.maxLevel = level;
-        }
-        return level;
     }
 
     // Find smallest element that is greater or equal to x
     public T ceiling(T x) {
-
-        find(x);
-        if(this.last[0].next[0] == this.tail) {
-            throw new NoSuchElementException();
+        //in case of empty list
+        if(isEmpty())
+        {
+           throw new NoSuchElementException();
         }
-        return (T) this.last[0].next[0].element;
+        if(!contains(x))//for the ceiling of the last element not present in the list e.g.(1,2,3,4) :ceiling(6)->exception
+        {
+            if (last[0].next[0] == tail)
+            {
+                throw  new NoSuchElementException();
+            }
+
+        }
+        return (T) last[0].next[0].element;
+    }
+
+    //Does list contains x?
+    public boolean contains(T x)
+    {
+        return containsWithSpan(x,new int[maxLevel]);
     }
 
     // Does list contain x?
-    public boolean contains(T x) {
-        find(x);
-        if(this.last[0].next[0].element == null) return false;
-        return x.compareTo((T)this.last[0].next[0].element) == 0;
-    }
-
-    // Return first element of list
-    public T first() {
-        return null;
-    }
-
-    // Find largest element that is less than or equal to x
-    public T floor(T x) {
-        return null;
-    }
-
-    // Return element at index n of list.  First element is at index 0.
-    public T get(int n) {
-        return getLog(n);
-//        return getLog(n);
-    }
-
-    // O(n) algorithm for get(n)
-    public T getLinear(int n) {
-        if (n < 0 || n >= this.size()) throw new NoSuchElementException();
-        Entry curr = this.head;
-        while(n>0) {
-            curr = curr.next[0];
-            n--;
+    public boolean containsWithSpan(T x,int[] steps_span) {
+        //return false in case of empty list
+        if (isEmpty())
+        {
+            return false;
         }
-        return (T) curr.element;
+        //to initialize last array call find
+        findWithSpan(x,steps_span);
+        if(last[0].next[0].element!=null && ((T)last[0].next[0].element).compareTo(x)==0)
+        {
+            return true;
+        }
+        return false;
     }
 
-    // Optional operation: Eligible for EC.
-    // O(log n) expected time for get(n). Requires maintenance of spans, as discussed in class.
-    public T getLog(int n) {
-        if(n < 0 || n>= this.size()) throw new NoSuchElementException();
-        Entry curr = this.head;
-        int level = this.maxLevel - 1;
-        while(n>0) {
-            if(curr.span[level] >= n) {
-                n-= curr.span[level];
-                level--;
+    //it will give the prev node of the node we want to find and it will fill up an array with the steps taken to reach current node
+    public void findWithSpan(T x,int[] steps_span)
+    {
+
+            Entry<T> p=head;
+            for (int i=maxLevel-1;i>=0;i--)
+            {
+                while(p.next[i].element!=null && ((T)p.next[i].element).compareTo(x)<0)//if next is lesser then x
+                {
+                    steps_span[i]+=p.span[i];
+                    p=p.next[i];
+
+                }
+                //if next is greater or equal x
+                last[i]=p;
             }
-            else
-                curr = curr.next[level];
+            //so when it comes down by one level-we will be having the number exactly same or greater hen the number we want to find
+            //so put it in last array
         }
-        return (T) curr.getElement();
+
+
+
+    //it will give the prev node of the node we want to find
+    public void find(T x)
+    {
+       findWithSpan(x,new int[maxLevel]);
     }
 
-    // Is the list empty?
-    public boolean isEmpty() {
-
-        return size() == 0;
-    }
-
-    // Iterate through the elements of list in sorted order
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    // Return last element of list
-    public T last() {
-        if(this.tail.prev.element!=null) return (T)this.tail.prev.element;
-        return null;
-    }
-
-    // Optional operation: Reorganize the elements of the list into a perfect skip list
-    // Not a standard operation in skip lists. Eligible for EC.
-    public void rebuild() {
-        int maxHeight = (int) Math.floor((Math.log(this.size)) / (Math.log(2))) + 1;
-        int[] heights = new int[this.size];
-        setHeights(heights, 0, this.size - 1, maxHeight);
-        Entry[] elements = new Entry[this.size];
-        copy(elements);
-        reset(heights, elements);
-        for (int i=1;i<=maxHeight;i++) {
-            build(i, heights, elements);
-        }
-    }
-
-    private void reset(int[] heights, Entry[] elements) {
-        setup();
-        this.head.next[0] = elements[0];
-        int n = heights.length;
-        for (int i=0;i<n;i++) {
-            elements[i].next = new Entry[heights[i]];
-            elements[i].span = new int[heights[i]];
-            if(i != heights.length - 1) {
-                elements[i].next[0] = elements[i+1];
-                elements[i].span[0] = 1;
+    //choose the level of the new node randomly-fast method
+    public int chooseLevel()
+    {
+        int lev=1+Integer.numberOfTrailingZeros(random.nextInt());
+        lev=Math.min(lev,maxLevel+1);//increase the maxLevel gradually
+        if(lev>maxLevel)
+        {
+            for(int i=maxLevel-1;i<lev;i++)
+            {
+                head.span[i]=size;
             }
+            maxLevel=lev;
         }
-        elements[n-1].next[0]= this.tail;
-    }
 
-    private void build(int level, int[] heights, Entry[] elements) {
-        int n = elements.length;
-        Entry prev = this.head;
-        int prevIdx = 0;
-        System.out.println(level);
-        for (int i=0;i<n;i++) {
-            if(heights[i] >= level) {
-                prev.next[level-1] = elements[i];
-                prev.span[level-1] = i - prevIdx;
-                prev = elements[i];
-                prevIdx = i;
-            }
-        }
-        prev.span[level-1] = n - prevIdx - 1;
-        prev.next[level-1] = this.tail;
+        return lev;
     }
-
-    private void copy(Entry[] elements) {
-        Entry curr = this.head.next[0];
-        for (int i=0;i<elements.length;i++) {
-            elements[i] = curr;
-            curr = curr.next[0];
-        }
-    }
-
-    private void setHeights(int[] heights, int l, int r, int mh) {
-        if(l > r) return;
-        if(l == r) {
-            heights[l] = mh;
-        }
-        int mid = (l+r) / 2;
-        heights[mid] = mh;
-        setHeights(heights, l, mid-1, mh-1);
-        setHeights(heights, mid+1, r, mh-1);
-    }
-
-    // Remove x from list.  Removed element is returned. Return null if x not in list
-    public T remove(T x) {
-        if(!contains(x)) return null;
-        Entry ent = this.last[0].next[0];
-        for (int i=0;i<ent.next.length;i++) {
-            this.last[i].next[i] = ent.next[i];
-        }
-        int i=0;
-        for (;i<ent.next.length;i++) {
-            this.last[i].span[i] += ent.span[i] - 1;
-        }
-        for(;i<maxLevel;i++) {
-            this.last[i].span[i]--;
-        }
-        this.size--;
-        return (T)ent.getElement();
-    }
-
-    // Return the number of elements in the list
-    public int size() {
-        return this.size;
-    }
-
 
     // Printing the contents of the list
     public void printList() {
@@ -315,4 +185,192 @@ public class SkipList<T extends Comparable<? super T>> {
         }
         System.out.println();
     }
+
+
+    // Return first element of list
+    public T first() {
+        if(isEmpty())//in case of empty list
+        {
+            throw new NoSuchElementException();
+        }
+        else
+        {
+            return (T)head.next[0].element;
+        }
+    }
+
+    // Find largest element that is less than or equal to x
+    public T floor(T x) {
+        //in case of empty list
+        if(isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
+        //for the floor of the first element not present in the list e.g.(4,5,6,7) :floor(3)->exception
+        if (!contains(x)) {
+            if (last[0] == head) {
+                throw new NoSuchElementException();
+
+            }
+        }
+        return (T)last[0].element;
+    }
+
+    // Return element at index n of list.  First element is at index 0.
+    public T get(int n) {
+        //in case of empty list
+        if(isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
+        return getLinear(n);
+    }
+
+    // O(n) algorithm for get(n)
+    public T getLinear(int n) {
+        if (n<0 || n>size-1)
+        {
+           throw new NoSuchElementException();
+        }
+        Entry<T> p=head;
+        for (int i =0;i<n;i++)
+        {
+            p=p.next[0];
+        }
+        return (T)p.next[0].element;
+    }
+
+    // Optional operation: Eligible for EC.
+    // O(log n) expected time for get(n). Requires maintenance of spans, as discussed in class.
+    public T getLog(int n) {
+        if (n<0 || n>size-1)
+        {
+            throw new NoSuchElementException();
+        }
+        Entry<T> p=head;
+        int count=n;
+        for (int i=maxLevel-1;i>=0;i--)
+        {
+            if(count>=p.span[i])
+            {
+                count-=p.span[i];
+                p=p.next[i];
+            }
+            if (count==0)
+                break;
+
+        }
+        return p.element;
+
+    }
+
+    // Is the list empty?
+    public boolean isEmpty() {
+        if(head.next[0]==tail)
+            return true;
+        return false;
+    }
+
+    // Iterate through the elements of list in sorted order
+
+    public Iterator<T> iterator() {
+        return (new SkipListIterator(this));
+    }
+
+    private class SkipListIterator implements Iterator<T> {
+        private  SkipList<T> list = null;
+        Entry<T> next = null;
+        Entry<T> cursor, prev;
+        boolean ready;
+
+        SkipListIterator(SkipList<T> list) {
+            this.list = list;
+            this.cursor = list.head;
+            ready = false;
+        }
+
+        public boolean hasNext() {
+            if (cursor.next[0]!=this.list.tail && cursor.next[0]!=null)
+                return true;
+            return false;
+
+        }
+
+        public T next() {
+            prev = cursor;
+            cursor =cursor.next[0];
+            ready = true;
+            return (T) cursor.element;
+        }
+
+        /*public void add(T ent) {
+            this.list.add((T) ent);
+        }*/
+        public void remove() {
+            if(!ready) {
+                throw new NoSuchElementException();
+            }
+            prev.next[0] = cursor.next[0];
+            // Handle case when tail of a list is deleted
+            if(cursor == list.tail) {
+                list.tail = prev;
+            }
+            cursor = prev;
+            ready = false;  // Calling remove again without calling next will result in exception thrown
+            list.size--;
+        }
+    }
+
+    // Return last element of list
+    public T last() {
+        if(head.next[0]==tail)//in case of empty list
+        {
+            throw new NoSuchElementException();
+        }
+        return getLinear(size-1);
+    }
+
+
+
+    // Optional operation: Reorganize the elements of the list into a perfect skip list
+    // Not a standard operation in skip lists. Eligible for EC.
+    public void rebuild() {
+
+    }
+
+    // Remove x from list.  Removed element is returned. Return null if x not in list
+    public T remove(T x) {
+        if (!contains(x))
+        {
+            return null;
+        }
+        Entry<T> ent=last[0].next[0];//element to be deleted
+
+        //till the height of the next
+        //put span=(span of last + span of the element to be deleted -1)
+        int i;
+        for (i=0;i<=ent.next.length-1;i++)
+        {
+            last[i].span[i]=(last[i].span[i]+ent.next[i].span[i]-1);//update span
+            last[i].next[i]=ent.next[i];//bypass ent at level i
+        }
+
+        //for the elements heigher then height of next of ent just set their span to - 1
+        for (i=ent.next.length;i<maxLevel;i++)
+        {
+           last[i].span[i]=last[i].span[i]-1;//update span
+        }
+
+        size=size-1;
+        return ent.element;
+
+    }
+
+    // Return the number of elements in the list
+    public int size() {
+        return size;
+    }
 }
+
+
+
